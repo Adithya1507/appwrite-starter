0d43c312808a31f31680f4e41e2e909951b81aec
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-15 12:46:57 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ import { Client,Databases,Query } from 'node-appwrite';
 import axios from 'axios';
 export default async ({ req, res, log, error }) => {
   try {
-      
-      
-      //onst body=req.body);
-      log("body"+req.body)
+
+    // the body contains details of the document created
       const collectionId = req.body.$collectionId;
       const documentId=req.body.$id;
       const databaseId=req.body.$databaseId;
-      log("colctnid"+collectionId)
+     
       // Initialize Appwrite client
       const client = new Client()
           .setEndpoint('https://cloud.appwrite.io/v1')
           .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-          //.setKey(process.env.APPWRITE_API_KEY);
+          
 
       
       const databases = new Databases(client);
@@ -29,37 +27,28 @@ export default async ({ req, res, log, error }) => {
       //   ]
       // );
   
-      // Update documents with name "adi" to set status field to "txn created"
-     // const updates = allDocuments.documents.map(async (document) => {
-          //if (document.name === 'adi') {
+        await databases.updateDocument(
+              databaseId, 
+              collectionId,
+              documentId,
+              { status: "txn commited" }
+          );
            
-              await databases.updateDocument(
-                 databaseId, // Use your database ID
-                  collectionId, // Use the extracted collection ID
-                  documentId,
-                  { status: "txn commited" }
-              );
-             // log(`Document with name ${document.name} updated.`);
-          //}
-      
-
-      // Wait for all updates to complete
-     // await Promise.all(updates);
           const documentDetails = await databases.getDocument(
           process.env.APPWRITE_FUNCTION_DATABASE_ID, 
           collectionId,
           documentId
           );
-          log("documentDetails"+documentDetails);
+          //checking if status was updated
           if(documentDetails.status=="txn commited")
             await triggerNotary1Function(databaseId,collectionId,documentId,log)
+            // await triggerNotary1Function(databaseId,collectionId,documentId,log)
+            // await triggerNotary1Function(databaseId,collectionId,documentId,log)
 
 
-      
-      // Send a response
-      return res.send('Documents updated successfully.');
-  } catch (error1) {
-      // If something goes wrong, log an error
+          return res.send('Documents updated successfully.');
+  }    catch (error1) {
+     
       error('Error updating documents: ' + error1);
       return res.send('Error updating documents.');
   }
@@ -75,7 +64,8 @@ async function triggerNotary1Function(databaseId,collectionId,documentId,log) {
       const dataToEncrypt ={
             "objToEncrypt" : payload
        }
-      // const encryptUrl= process.env.encrypt-url
+       const encryptUrl1= process.env.encrypt-url
+       log("encryptUrl1: "+encryptUrl1)
       const encryptUrl='https://65cca595704ed3c371af.appwrite.global/'
       log("encryptUrl"+encryptUrl)
       const encryptedData=await axios.post(encryptUrl,dataToEncrypt);
