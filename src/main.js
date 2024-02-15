4e3667e629fbd9fa2c011ea016cd1ac50321aa60
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-15 13:08:59 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,8 +13,6 @@ export default async ({ req, res, log, error }) => {
           .setEndpoint('https://cloud.appwrite.io/v1')
           .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
           
-
-      
       const databases = new Databases(client);
 
       // Get documents from the specified collection with name "adi"
@@ -34,28 +32,31 @@ export default async ({ req, res, log, error }) => {
               { status: "txn commited" }
           );
            
-          const documentDetails = await databases.getDocument(
-          process.env.APPWRITE_FUNCTION_DATABASE_ID, 
-          collectionId,
-          documentId
-          );
-          //checking if status was updated
-          if(documentDetails.status=="txn commited")
+        const documentDetails = await databases.getDocument(
+        process.env.APPWRITE_FUNCTION_DATABASE_ID, 
+        collectionId,
+        documentId
+        );
+        //checking if status was updated
+        if(documentDetails.status=="txn commited")
             await triggerNotary1Function(databaseId,collectionId,documentId,log)
             // await triggerNotary1Function(databaseId,collectionId,documentId,log)
             // await triggerNotary1Function(databaseId,collectionId,documentId,log)
 
 
           return res.send('Documents updated successfully.');
-  }    catch (error1) {
+  }catch (error1) {
      
       error('Error updating documents: ' + error1);
       return res.send('Error updating documents.');
   }
 };
+
+
+
 async function triggerNotary1Function(databaseId,collectionId,documentId,log) {
-    try {
-      const webhookUrl = 'https://65cca72dd29fc671edf8.appwrite.global/'; 
+  try {
+     
       const payload = {
         databaseId: databaseId,
         collectionId: collectionId,
@@ -65,17 +66,13 @@ async function triggerNotary1Function(databaseId,collectionId,documentId,log) {
             "objToEncrypt" : payload
        }
        const encryptUrl= process.env.encrypt_url;
-      
-     // const encryptUrl='https://65cca595704ed3c371af.appwrite.global/';
-      log("encryptUrl"+encryptUrl)
       const encryptedData=await axios.post(encryptUrl,dataToEncrypt);
-      log("encryptedData: " + JSON.stringify(encryptedData.data));
-      const data = encryptedData.data;
-      const cipherText = data.encryptObject.ciphertext;
-      log("cipherText"+ cipherText)
-      await axios.post(webhookUrl,cipherText);
-      log('Webhook triggered in the target project.');
-    } catch (error1) {
-      console.log('Error triggering webhook in the target project: ' + error1.message);
+      const cipherText = encryptedData.data.encryptObject.ciphertext;
+
+      const notaryurl1 = process.env.notary1_url; 
+      await axios.post(notaryurl1,cipherText);
+
+  } catch (error1) {
+      log('Error triggering webhook in the target project: ' + error1.message);
     }
   }
