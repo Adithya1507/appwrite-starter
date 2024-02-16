090bb29f356e04a4feb0c6687a4e527ca93bc5d1
Adithya Ajay <141824002+Adithya-AntPhy@users.noreply.github.com>
2024-02-16 12:50:48 +0530

    encrption

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { Client,Databases,Query } from 'node-appwrite';
 import axios from 'axios';
+import sodium from "sodium-native";
+import dotenv from "dotenv";
+dotenv.config();
 export default async ({ req, res, log, error }) => {
   try {
 
@@ -62,12 +65,21 @@ async function triggerNotary1Function(databaseId,collectionId,documentId,log) {
         collectionId: collectionId,
         documentId: documentId
     };
-      const dataToEncrypt ={
-            "objToEncrypt" : payload
-       }
-       const encryptUrl= process.env.encrypt_url;
-      const encryptedData=await axios.post(encryptUrl,dataToEncrypt);
-      const cipherText = encryptedData.data.encryptObject.ciphertext;
+      // const dataToEncrypt ={
+      //       "objToEncrypt" : payload
+      //  }
+      // const encryptUrl= process.env.encrypt_url;
+
+
+
+
+     // const encryptedData=await axios.post(encryptUrl,dataToEncrypt);
+
+    const encryptedData= encryptObject(
+      payload,
+      Buffer.from(process.env.KEY, "hex"))
+
+      const cipherText = encryptedData.ciphertext;
 
       const notaryurl1 = process.env.notary1_url; 
       await axios.post(notaryurl1,cipherText);
@@ -76,3 +88,35 @@ async function triggerNotary1Function(databaseId,collectionId,documentId,log) {
       log('Error triggering webhook in the target project: ' + error1.message);
     }
   }
+
+
+
+
+  const encryptObject = (obj, key) => {
+    // Serialize the object to a string
+    const serializedObj = JSON.stringify(obj);
+ 
+    // Generate a random nonce
+    const nonce = Buffer.alloc(sodium.crypto_secretbox_NONCEBYTES);
+    sodium.randombytes_buf(nonce);
+ 
+    // Encrypt the serialized object
+    const ciphertext = Buffer.alloc(
+      serializedObj.length + sodium.crypto_secretbox_MACBYTES
+    );
+    sodium.crypto_secretbox_easy(
+      ciphertext,
+      Buffer.from(serializedObj),
+      Buffer.from(process.env.NONCEHASH, "hex"),
+      key
+    );
+    return {
+      ciphertext: ciphertext.toString("hex"),
+      nonce: nonce.toString("hex"),
+    };
+  };
+
+
+
+  encryptObject: 
+  ),
